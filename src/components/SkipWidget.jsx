f426d12a9b733d1d51cb62ca43a8689ec4fbfc43
MisterLoops <114404546+MisterLoops@users.noreply.github.com>
2025-09-02 14:27:21 +0200

    Revert "Test netlify proxy"
    This reverts commit 3262b16e76654f77a1b485ba07541ca50e8a7d3c.

--- src/components/SkipWidget.jsx
+++ src/components/SkipWidget.jsx
@@ -13,7 +13,9 @@ const SkipWidget = ({
 
     useEffect(() => {
         setApiOptions({
-            apiURL: '/api/skip',
+            apiURL: process.env.NODE_ENV === 'development' 
+                ? 'http://localhost:8888/.netlify/functions/skip-proxy'  // Netlify dev server
+                : '/.netlify/functions/skip-proxy',  
             cumulativeAffiliateFeeBps: "200",
             chainIdsToAffiliates: {
                 "interwoven-1": {
